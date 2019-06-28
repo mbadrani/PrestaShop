307dffa8c13d01b92496afcba4d80863daf368f9
Simon Garny <simon.garny@prestashop.com>
2019-06-28 16:11:49 +0200

    updating link checker

--- tests/E2E-Puppeteer/test/linkchecker.js
+++ tests/E2E-Puppeteer/test/linkchecker.js
@@ -1,67 +1,153 @@
 'use strict';
 
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 require('events').EventEmitter.prototype._maxListeners = Infinity;
 
-let argv = require('minimist')(process.argv.slice(2));
 
-let URLBO = argv.URLBO || 'http://localhost/prestashop_reach75/admin-dev';
-let EMAIL = argv.EMAIL || 'demo@prestashop.com';
-let PASSWORD = argv.PASSWORD || 'prestashop_demo';
+let URL_FO = process.env.URL_FO || 'http://localhost/prestashop/';
+let URL_BO = process.env.URL_BO || 'http://localhost/prestashop/admin-dev/';
+let EMAIL = process.env.LOGIN || 'demo@prestashop.com';
+let PASSWORD = process.env.PASSWD || 'prestashop_demo';
+let HEADLESS = process.env.HEADLESS || true;
+let responses = [];
+let number_requests = {BO:0, FO:0};
 
-const getAllUrl = async (browser, page) => {
+let selector_BO = 'nav.nav-bar.d-none.d-md-block ul li a.link[href]';
+let selector_FO = 'a[href]';
 
-	const hrefs = await page.evaluate(
-    () => Array.from(document.body.querySelectorAll('nav.nav-bar.d-none.d-md-block ul li a.link[href]'), ({ href }) => href)
-    );
+let office = 'BO';
+let cur_href = null;
 
-	return hrefs
+const report_path = 'reports/';
+
+let object = {
+  stats: {
+    start: new Date().getTime(),
+    end:null,
+  },
+  BO : {
+    totalLinks: 0,
+    totalRequests: 0,
+    passed: [],
+    failed: []
+  },
+  FO : {
+    totalLinks: 0,
+    totalRequests: 0,
+    passed: [],
+    failed: []
+  },
+};
+
+const getAllUrl = async (page, selector) => {
+  return await page.$$eval(selector, as => as.map(a => a.href))
+};
+
+
+const interceptRequestAndResponse = async(page) => {
+  await page.setRequestInterception(true);
+  await page.on('request', request => {
+    request.continue();
+  });
+  await page.on('response', response => {
+    const request = response.request();
+    const url = request.url();
+    const status = response.status().toString();
+
+    number_requests[office] += 1;
+
+    if(status.startsWith('4') || status.startsWith('5')) {
+      console.log(' * Failed response in '+office+' from '+cur_href+': ' + url + ' (' + status+')');
+      object[office].failed.push({page: cur_href, URL : url , status : status});
+    } else {
+      responses.push({url: url, status: status});
+    }
+  });
 }
 
+const checkStatusUrls = async (page, hrefs) => {
+  //check urls
+  for (const href of hrefs) {
+    cur_href = href;
+    object[office].passed.push({url: href, date: new Date().getTime(), responses: responses});
 
-const checkStatusUrls = async (browser, page, hrefs) => {
+    if (!href.includes('mailto:') && href.includes(URL_FO)) {
+      await page.goto(href, {waitUntil: 'domcontentloaded'});
+    }
+    responses = [];
+  }
 
-	for (const href of hrefs) {
-    await page.goto(href, { waitUntil: 'domcontentloaded' }).catch(e => console.error(e))
-    page.on('response', response => {
-    	if(response.status().toString().startsWith("4") || response.status().toString().startsWith("5")) {
-    		const urlStatus = ["--HTTP CODE--", response.status(),"URL:" ,response.url(), "From:", href];
-    	//	await page.evaluate(()=> {throw new Error('page error catched:'+ urlStatus)});
-      //  throw new Error('page error catched:'+ urlStatus)
-      	throw ('page error catched:'+ urlStatus)
-      	}
-    	});
-	}
-}
+  object[office].totalRequests = number_requests[office];
+};
 
+const login_BO = async (page) => {
+  await page.type('#email', EMAIL);
+  await page.type('#passwd', PASSWORD);
+  await page.click('#submit_login');
+  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+};
 
 const run = async () => {
+  let browser = await puppeteer.launch({ headless: HEADLESS });
+  let page = await browser.newPage();
 
-	const browser = await puppeteer.launch({ headless: false })
-	const page = await browser.newPage()
+  await interceptRequestAndResponse(page, office);
 
-	await page.goto(URLBO, { waitUntil: 'networkidle0' });
-	await page.type('#email', EMAIL)
-	await page.type('#passwd', PASSWORD)
-  await page.click('#submit_login')
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
-	/*
-	await page.waitForSelector('#subtab-AdminCatalog')
-	const subtab = await page.$eval('#subtab-AdminCatalog a.link[href]', ({ href }) => href);
-	console.log(subtab);
-	*/
-//	await page.click('#subtab-AdminCatalog')
+  console.time('Check urls ');
+  //Start testing BO
+  console.log('Begin testing BO');
+  await page.goto(URL_BO, { waitUntil: 'networkidle0' });
+  await login_BO(page);
+  let urlList = await getAllUrl(page, selector_BO);
+  console.log(' - '+urlList.length+' URL to crawl');
+  office = 'BO';
+  object.BO.totalLinks = urlList.length;
+  await checkStatusUrls(page, urlList);
+  console.log('End testing BO');
+  // End testing BO
 
-  const urlList = await getAllUrl(browser, page)
+  // Start testing FO
+  console.log('Begin testing FO');
+  await page.goto(URL_FO, { waitUntil: 'networkidle0' });
+  urlList = await getAllUrl(page, selector_FO);
+  console.log(' - '+urlList.length+' URL to crawl');
+  office = 'FO';
+  object.FO.totalLinks = urlList.length;
+  await checkStatusUrls(page, urlList);
+  console.log('End testing FO');
+  // End testing FO
 
-  const checkUrls = await checkStatusUrls(browser, page, urlList)
+  browser.close();
+  console.timeEnd('Check urls ');
 
-  browser.close()
-}
+  object.stats.end = new Date().getTime();
+
+  const filename = 'report_'+new Date().toJSON().slice(0,10)+'.json';
+  fs.writeFile(report_path+filename, JSON.stringify(object), function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("File "+report_path+filename+" saved!");
+  });
+
+  //is there any error when crawling ?
+  if (object.BO.failed.length > 0 || object.FO.failed.length > 0) {
+    let error_message = '';
+    if (object.BO.failed.length > 0) {
+      error_message += ' - '+object.BO.failed.length+" errors in BO";
+    }
+    if (object.FO.failed.length > 0) {
+      error_message += ' - '+object.FO.failed.length+" errors in FO";
+    }
+    throw ('Errors caught : '+ error_message);
+
+  }
+
+};
 
 
 run()
   .then(value => {
-    console.log("--------everything is fine ... the end :-* --------")
-  })
-  .catch(e => console.log(`error: ${e}`))
+    console.log("--------the end--------")
+  }).catch(e => console.log(`error: ${e}`));
