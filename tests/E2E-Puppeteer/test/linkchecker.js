f97900c3f08f724c33fdca38b75f2411240a9b4a
nesrine abdmouleh <nesrine.abdmouleh@prestashop.com>
2019-07-05 13:20:20 +0100

    Reformat code

--- tests/E2E-Puppeteer/test/linkchecker.js
+++ tests/E2E-Puppeteer/test/linkchecker.js
@@ -1,10 +1,9 @@
 'use strict';
 
+require('events').EventEmitter.prototype._maxListeners = Infinity;
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-require('events').EventEmitter.prototype._maxListeners = Infinity;
-
-
+const report_path = './reports/';
 //maybe add tracing option : https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#class-tracing
 let URL_FO = process.env.URL_FO || 'http://localhost:8080/';
 let URL_BO = process.env.URL_BO || 'http://localhost:8080/admin-dev/';
@@ -12,28 +11,23 @@ let EMAIL = process.env.LOGIN || 'demo@prestashop.com';
 let PASSWD = process.env.PASSWD || 'prestashop_demo';
 let HEADLESS = process.env.HEADLESS || true;
 let responses = [];
-let number_requests = {BO:0, FO:0};
-
+let number_requests = {BO: 0, FO: 0};
 let selector_BO = 'nav.nav-bar.d-none.d-md-block ul li a.link[href]';
 let selector_FO = 'a[href]';
-
 let office = 'BO';
 let cur_href = null;
-
-const report_path = './reports/';
-
 let object = {
   stats: {
     start: new Date().getTime(),
-    end:null,
+    end: null,
   },
-  BO : {
+  BO: {
     totalLinks: 0,
     totalRequests: 0,
     passed: [],
     failed: []
   },
-  FO : {
+  FO: {
     totalLinks: 0,
     totalRequests: 0,
     passed: [],
@@ -41,12 +35,11 @@ let object = {
   },
 };
 
-const getAllUrl = async (page, selector) => {
+const getAllUrls = async (page, selector) => {
   return await page.$$eval(selector, as => as.map(a => a.href))
 };
 
-
-const interceptRequestAndResponse = async(page) => {
+const interceptRequestAndResponse = async (page) => {
   await page.setRequestInterception(true);
   await page.on('request', request => {
     request.continue();
@@ -55,12 +48,10 @@ const interceptRequestAndResponse = async(page) => {
     const request = response.request();
     const url = request.url();
     const status = response.status().toString();
-
     number_requests[office] += 1;
-
-    if(status.startsWith('4') || status.startsWith('5')) {
-      console.log(' * Failed response in '+office+' from '+cur_href+': ' + url + ' (' + status+')');
-      object[office].failed.push({page: cur_href, URL : url , status : status});
+    if (status.startsWith('4') || status.startsWith('5')) {
+      console.log(' * Failed response in ' + office + ' from ' + cur_href + ': ' + url + ' (' + status + ')');
+      object[office].failed.push({page: cur_href, URL: url, status: status});
     } else {
       responses.push({url: url, status: status});
     }
@@ -78,7 +69,6 @@ const checkStatusUrls = async (page, hrefs) => {
     }
     responses = [];
   }
-
   object[office].totalRequests = number_requests[office];
 };
 
@@ -86,7 +76,7 @@ const login_BO = async (page) => {
   await page.type('#email', EMAIL);
   await page.type('#passwd', PASSWD);
   await page.click('#submit_login');
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  await page.waitForNavigation({waitUntil: 'domcontentloaded'});
 };
 
 const run = async () => {
@@ -95,16 +85,15 @@ const run = async () => {
     args: ['--no-sandbox']
   });
   let page = await browser.newPage();
-
   await interceptRequestAndResponse(page, office);
-
   console.time('Check urls ');
+
   //Start testing BO
   console.log('Begin testing BO');
-  await page.goto(URL_BO, { waitUntil: 'networkidle0' });
+  await page.goto(URL_BO, {waitUntil: 'networkidle0'});
   await login_BO(page);
-  let urlList = await getAllUrl(page, selector_BO);
-  console.log(' - '+urlList.length+' URL to crawl');
+  let urlList = await getAllUrls(page, selector_BO);
+  console.log(' - ' + urlList.length + ' URL to crawl');
   office = 'BO';
   object.BO.totalLinks = urlList.length;
   await checkStatusUrls(page, urlList);
@@ -113,9 +102,9 @@ const run = async () => {
 
   // Start testing FO
   console.log('Begin testing FO');
-  await page.goto(URL_FO, { waitUntil: 'networkidle0' });
-  urlList = await getAllUrl(page, selector_FO);
-  console.log(' - '+urlList.length+' URL to crawl');
+  await page.goto(URL_FO, {waitUntil: 'networkidle0'});
+  urlList = await getAllUrls(page, selector_FO);
+  console.log(' - ' + urlList.length + ' URL to crawl');
   office = 'FO';
   object.FO.totalLinks = urlList.length;
   await checkStatusUrls(page, urlList);
@@ -127,29 +116,26 @@ const run = async () => {
 
   object.stats.end = new Date().getTime();
 
-  const filename = 'report_'+new Date().toJSON().slice(0,10)+'.json';
-  fs.writeFile(report_path+filename, JSON.stringify(object), function(err) {
-    if(err) {
+  const filename = 'report_' + new Date().toJSON().slice(0, 10) + '.json';
+  fs.writeFile(report_path + filename, JSON.stringify(object), function (err) {
+    if (err) {
       return console.log(err);
     }
-    console.log("File "+report_path+filename+" saved!");
+    console.log("File " + report_path + filename + " saved!");
   });
 
   //is there any error when crawling ?
   if (object.BO.failed.length > 0 || object.FO.failed.length > 0) {
     let error_message = '';
     if (object.BO.failed.length > 0) {
-      error_message += ' - '+object.BO.failed.length+" errors in BO";
+      error_message += ' - ' + object.BO.failed.length + " errors in BO";
     }
     if (object.FO.failed.length > 0) {
-      error_message += ' - '+object.FO.failed.length+" errors in FO";
+      error_message += ' - ' + object.FO.failed.length + " errors in FO";
     }
-    throw ('Errors caught : '+ error_message);
-
+    throw ('Errors caught : ' + error_message);
   }
-
 };
-
 
 run()
   .then(() => {
